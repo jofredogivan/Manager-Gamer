5bdac977c9ecda79935f1357a77ea8e52e2d1d54
jofredogivan <jofredogivan@gmail.com>
2025-07-05 15:09:17 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,13 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("fmanager-cache").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        // Caminho corrigido para main.js
-        "/js/main.js", 
-        "/js/ui.js",
-        "/js/core.js",
-        "/js/team.js", // <-- MANTENHA se existir, REMOVA se não existir
-        "/js/match.js",
-        "/js/cup.js",
-        // Caminhos para partials (assumindo que estão na pasta partials na raiz)
-        "/partials/elenco.html",
-        "/partials/taticas.html",
-        "/partials/partida.html"
-      ]);
-    })
-  );
+// service-worker.js
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('activate', event => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // Por enquanto não interceptamos nada
 });
