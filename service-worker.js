8d616b050c344598987754746d71d8895ef7c326
jofredogivan <jofredogivan@gmail.com>
2025-07-05 15:27:49 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,9 @@
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
-
 self.addEventListener('activate', event => {
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
-  // Nenhuma interceptação
+  // Nenhum cache ainda
 });
