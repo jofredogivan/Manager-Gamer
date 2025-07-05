d689a9fbecf889191051f773b39c559e6d074a14
jofredogivan <jofredogivan@gmail.com>
2025-07-05 21:19:50 +0000

    Update reputation.js

--- js/reputation.js
+++ js/reputation.js
@@ -1,19 +1,8 @@
-// js/cup.js
+// js/reputation.js
 
 import { myTeam } from "./team.js";
-import { clubesSerieB } from "./clubes.js";
 
-export function iniciarCopa(timePrincipal) {
-  const adversario = clubesSerieB[Math.floor(Math.random() * clubesSerieB.length)];
-  const resultado = Math.random();
-  let mensagem = "";
-
-  if (resultado > 0.5) {
-    mensagem = `🏆 Vitória contra ${adversario} na Copa!`;
-    myTeam.money += 10000;
-  } else {
-    mensagem = `❌ Derrota para ${adversario}. Eliminado na 1ª fase da Copa.`;
-  }
-
-  alert(mensagem);
+export function aumentarReputacao(pontos) {
+  myTeam.reputation += pontos;
+  if (myTeam.reputation > 100) myTeam.reputation = 100;
 }
