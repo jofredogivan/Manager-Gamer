e4779da1ff92b9b78f639adc7d794fb7640fff3b
jofredogivan <jofredogivan@gmail.com>
2025-09-06 07:32:04 -0300

    Delete js directory

--- js/reputation.js
+++
@@ -1,8 +0,0 @@
-// js/reputation.js
-
-import { myTeam } from "./team.js";
-
-export function aumentarReputacao(pontos) {
-  myTeam.reputation += pontos;
-  if (myTeam.reputation > 100) myTeam.reputation = 100;
-}
